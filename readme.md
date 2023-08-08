3e84c77200d7c68b75a8966fef924d10de0e4355
Cristiano <cristiano123@gmail.com>
2023-08-08 19:29:24 -0300

    First commit, tool is functional

---
+++ readme.md
@@ -0,0 +1,11 @@
+# PWW's tool to add the page logo into images!
+
+Made with HTML, CSS and JavaScript. For personal use.
+
+## How it works:
+
+Select an image from your device.<br/>
+Choose the logo's position.<br/>
+If it's all good, download the image.
+
+Made by Cristiano.
---
+++ script.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const imageInput = document.getElementById("imageInput");
+    const positionButtons = document.querySelectorAll(".positionButton");
+    const outputContainer = document.getElementById("outputContainer");
+    const outputImage = document.getElementById("outputImage");
+  
+    positionButtons.forEach(button => {
+      button.addEventListener("click", function() {
+        applyWatermark(button.dataset.position, button.dataset.size);
+      });
+    });
+  
+    function applyWatermark(position, size) {
+      const selectedImage = imageInput.files[0];
+      if (selectedImage) {
+        const reader = new FileReader();
+  
+        reader.onload = function(event) {
+          const img = new Image();
+          img.src = event.target.result;
+  
+          img.onload = function() {
+            const canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+  
+            const context = canvas.getContext("2d");
+            context.drawImage(img, 0, 0);
+  
+            const logo = new Image();
+            logo.src = "logo.png"; // Path to your logo image
+            let logoSize, padding;
+  
+            switch (size) {
+              case "small":
+                logoSize = 100;
+                padding = 5;
+                break;
+              case "medium":
+                logoSize = 200;
+                padding = 10;
+                break;
+            }
+  
+            logo.onload = function() {
+              let logoX, logoY;
+  
+              switch (position) {
+                case "bottom-left":
+                  logoX = padding;
+                  logoY = canvas.height - logoSize - padding;
+                  break;
+                case "top-left":
+                  logoX = padding;
+                  logoY = padding;
+                  break;
+                case "bottom-right":
+                  logoX = canvas.width - logoSize - padding;
+                  logoY = canvas.height - logoSize - padding;
+                  break;
+                case "top-right":
+                  logoX = canvas.width - logoSize - padding;
+                  logoY = padding;
+                  break;
+                case "center":
+                  logoX = (canvas.width - logoSize) / 2;
+                  logoY = (canvas.height - logoSize) / 2;
+                  break;
+              }
+  
+              context.drawImage(logo, logoX, logoY, logoSize, logoSize);
+  
+              outputImage.src = canvas.toDataURL();
+              outputContainer.style.display = "block";
+            };
+          };
+        };
+  
+        reader.readAsDataURL(selectedImage);
+      }
+    }
+  });
+  
